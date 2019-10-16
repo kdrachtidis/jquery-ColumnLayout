fe46f39dd3e95af111ae6686b4ba2bb4da5af87f
Konstantinos Drachtidis <kdrachtidis@users.noreply.github.com>
2019-10-16 02:14:55 +0200

    Left container replaced with JSON

--- js/import-xml.js
+++ js/import-xml.js
@@ -7,7 +7,7 @@ function leftSidebar(xml) {
     var count = $(xml).find("category").size();
     $("#drop-groupslist .label, #left-sidebar .title .label").append(title);
     $("#drop-groupslist .count, #left-sidebar .title .count").append(" (" + count + ")");
-    $(xml).find("category").each(function() {
+    $(xml).find("category").each(function () {
         var header = $(this).find("header").text();
         var sheader = header.substr(0, 16);
         $('<li><h1><a href="#">' + title + '</a></h1></li>').appendTo("#groupslist ol");
@@ -24,31 +24,24 @@ function rightSidebar(xml) {
     var count = $(xml).find("source").size();
     $("#right-sidebar .title .label").append(title);
     $("#right-sidebar .title .count").append(" (" + count + ")");
-    $(xml).find("source").each(function() {
+    $(xml).find("source").each(function () {
         var header = $(this).find("header").text();
         $('<li><a href="#">' + header + '</a></li>').appendTo("#resources ol");
     });
 }
 
-function leftContainer(xml) {
-    var title = $(xml).find("list").attr("title");
-    var count = $(xml).find("element").size();
-    $("#drop-masterlist .label, #left-container .title .label").append(title);
-    $("#drop-masterlist .count, #left-container .title .count").append(" (" + count + ")");
-    $(xml).find("element").each(function() {
-        var header = $(this).find("header").text();
-        var sheader = header.substr(0, 25);
-        var subheader = $(this).find("subheader").text();
-        var kpi = $(this).find("kpi").text();
-        var attribute1 = $(this).find("attribute1").text();
-        var attribute2 = $(this).find("attribute2").text();
-        var attribute3 = $(this).find("attribute3").text();
-        var attribute4 = $(this).find("attribute3").text();
-        $('<article class="list-object"><aside>' + subheader + '</aside><header><h2>' + header + '</h2><h3>' + kpi + '</h3></header><footer><ul class="switch"><li class="more"><button type="button">more</button></li><li class="less"><button type="button">less</button></li></ul><ul class="attributes"><li>' + attribute1 + '</li><li class="right">' + attribute2 + '</li><li>' + attribute3 + '</li><li class="right">' + attribute4 + '</li></ul></footer></article>').appendTo("#masterlist");
-        if (header.length < 26) {
-            $('<option>' + header + '</option>').appendTo("#drop-masterlist select");
+function leftContainerJSON(data) {
+    var dCount = data.items.length;
+    var dTitle = data.title;    
+    $("#drop-masterlist .label, #left-container .title .label").append(dTitle);
+    $("#drop-masterlist .count, #left-container .title .count").append(" (" + dCount + ")");
+
+    $(data.items).each(function (index) {
+        $('<article class="list-object"><aside>' + this.subheader + '</aside><header><h2>' + this.header + '</h2><h3>' + this.kpi + '</h3></header><footer><ul class="switch"><li class="more"><button type="button">more</button></li><li class="less"><button type="button">less</button></li></ul><ul class="attributes"><li>' + this.attribute1 + '</li><li class="right">' + this.attribute2 + '</li><li>' + this.attribute3 + '</li><li class="right">' + this.attribute4 + '</li></ul></footer></article>').appendTo("#masterlist");
+        if (this.header.length < 26) {
+            $('<option>' + this.header + '</option>').appendTo("#drop-masterlist select");
         } else {
-            $('<option>' + sheader + '...</option>').appendTo("#drop-masterlist select");
+            $('<option>' + this.header.substr(0, 25) + '...</option>').appendTo("#drop-masterlist select");
         }
     });
 }
@@ -58,7 +51,7 @@ function rightContainer(xml) {
     var count = $(xml).find("entry").size();
     $("#right-container .title .label").append(title);
     $("#right-container .title .count").append(" (" + count + ")");
-    $(xml).find("entry").each(function() {
+    $(xml).find("entry").each(function () {
         var header = $(this).find("title").text();
         var date = $(this).find("date").text();
         var time = $(this).find("timestamp").text();
@@ -73,27 +66,27 @@ function mainContainer(xml) {
     var subheader = $(xml).find("header2").text();
     $("span.header-1").append(header);
     $("span.header-2").append(subheader);
-    $(xml).find("kpi").each(function() {
+    $(xml).find("kpi").each(function () {
         var kpi = $(this).text();
         var label = $(this).attr("title");
         $('<li><span class="kpi-amount">' + kpi + '</span><span class="kpi-title">' + label + '</span></li>').appendTo("#objectheader div.kpi ol");
     });
-    $(xml).find("attribute").each(function() {
+    $(xml).find("attribute").each(function () {
         var attribute = $(this).text();
         $('<li>' + attribute + '</li>').appendTo("#objectheader div.attribute ol");
     });
-    $(xml).find("status").each(function() {
+    $(xml).find("status").each(function () {
         var status = $(this).text();
         $('<li>' + status + '</li>').appendTo("#objectheader div.status ol");
     });
-    $(xml).find("tabcontainer").each(function() {
+    $(xml).find("tabcontainer").each(function () {
         var tabcontainer = $(this).attr("tab");
         $('<li><a href="#">' + tabcontainer + '</a></li>').appendTo("#tabMenu ol");
     });
 }
 
 function simpleForm(xml) {
-    $(xml).find("entry").each(function() {
+    $(xml).find("entry").each(function () {
         var label = $(this).attr("label");
         var amount = $(this).text();
         $('<p class="form-entry"><span class="label">' + label + '</span><span class="amount">' + amount + '</span></p>').appendTo(".form-content");
@@ -116,10 +109,9 @@ function getXML() {
         error: error
     });
     $.ajax({
-        type: "GET",
-        url: "./xml/leftContainerContent.xml",
-        dataType: "xml",
-        success: leftContainer,
+        dataType: "json",
+        url: "./json/leftContainerContent.json",
+        success: leftContainerJSON,
         error: error
     });
     $.ajax({
@@ -145,6 +137,6 @@ function getXML() {
     });
 }
 
-$(document).ready(function() {
+$(document).ready(function () {
     getXML();
 });
